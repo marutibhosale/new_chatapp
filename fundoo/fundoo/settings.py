0391423430d6e5e979f21b39c2b72f0a7519a1e0
marutibhosale <59178777+marutibhosale@users.noreply.github.com>
2021-02-25 13:52:32 +0530

    Modified:setting.py added database setting

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -103,13 +103,14 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbname',
-        'USER': 'user',
-        'PASSWORD': 'dsfdsfbdspfus',
-        'HOST': 'endpoint.google.com',
-        'PORT': '1234',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'password123',
+        'HOST': 'database-1.cbxgvybefbx8.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
 #sagar kadam
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
