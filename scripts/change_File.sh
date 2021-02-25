e5022b394c864a4a53753105d8a6215f085c1ed5
marutibhosale <59178777+marutibhosale@users.noreply.github.com>
2021-02-25 16:59:11 +0530

    modified:use env variable for database

--- scripts/change_File.sh
+++ scripts/change_File.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
+export dbname=postgres
+export dbpassword=password123
+export dbuser=postgres
+export dbhost=database-1.cbxgvybefbx8.ap-south-1.rds.amazonaws.com
 sudo systemctl daemon-reload
 sudo systemctl enable gunicorn
