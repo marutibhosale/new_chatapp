f5b7321728f560a553555dd4de64d61420473544
marutibhosale <59178777+marutibhosale@users.noreply.github.com>
2021-02-25 09:57:37 +0530

    Add:appspec.yml

---
+++ scripts/change_File.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo systemctl daemon-reload
+sudo systemctl enable gunicorn
+sudo ln -s /etc/nginx/sites-available/fundo /etc/nginx/sites-enabled
+sudo rm /etc/nginx/sites-enabled/default
