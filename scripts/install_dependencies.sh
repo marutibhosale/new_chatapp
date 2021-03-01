755286278ee310c48281e7c38366469222ce07a3
marutibhosale <61083245+marutibhosale@users.noreply.github.com>
2021-03-01 13:33:32 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pip3 install django bcrypt django-extensions
-pip3 install -r /home/ubuntu/new_chatapp/requirements.txt
+pip3 install -r /home/ec2-user/new_chatapp/requirements.txt
 pip3 install gunicorn
